55ee2b053b275befd82f9d49e6eb93ca811724a9
TheNewStyles <tyler.peterson@utah.edu>
2018-08-09 15:21:18 -0500

    Adjust nav menu

--- src/Components/MenuNav.js
+++ src/Components/MenuNav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { Button, Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
 
 import '../Styles/MenuNav.css'
 
@@ -13,44 +13,43 @@ export default class SidebarExampleDimmed extends Component {
   render() {
     const { visible } = this.state
 
-    return (
-      <div>
-        <Button onClick={this.handleButtonClick}>Toggle visibility</Button>
-        <div >     
-          <Sidebar.Pushable as={Segment}>
-            <Sidebar
-              as={Menu}
-              animation='overlay'
-              icon='labeled'
-              inverted
-              onHide={this.handleSidebarHide}
-              vertical
-              visible={visible}
-              width='thin'
-            >
-              <Menu.Item as='a'>
-                <Icon name='home' />
-                Home
-              </Menu.Item>
-              <Menu.Item as='a'>
-                <Icon name='star' />
-                Projects
-              </Menu.Item>
-              <Menu.Item as='a'>
-                <Icon name='camera' />
-                Contact
-              </Menu.Item>
-            </Sidebar>
+    return (     
+      <div >     
+      <Button id="hamburger" onClick={this.handleButtonClick}>-<br/>-<br/>-</Button>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='star' />
+              Projects
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Contact
+            </Menu.Item>
+          </Sidebar>
 
-            <Sidebar.Pusher dimmed={visible}>
-              <Segment basic>
-                <Header as='h3'>Application Content</Header>
-                <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-              </Segment>
-            </Sidebar.Pusher>
-          </Sidebar.Pushable>
-        </div>
+          <Sidebar.Pusher dimmed={visible}>
+            {/* <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Segment> */}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
+      
     )
   }
 }
