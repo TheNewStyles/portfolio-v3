30c847afcacacb3cbb5dff91f2a0e6c01a90a284
TheNewStyles <tyler.peterson@utah.edu>
2018-08-09 10:48:15 -0500

    Start playing around with semantic UI

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import '../Styles/App.css';
+import NavMenu from './Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <div className="welcome-section">
+          <NavMenu />
+        </div>
       </div>
     );
   }
